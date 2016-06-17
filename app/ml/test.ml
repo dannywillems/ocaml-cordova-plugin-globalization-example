0b4e08c2a6819ab2c8999601a4a1c71781ede13d
Danny Willems <contact@danny-willems.be>
2016-06-17 16:53:37 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,3 +1,5 @@
+module G = Cordova_globalization
+
 let doc = Dom_html.document
 
 let create_p s =
@@ -6,58 +8,64 @@ let create_p s =
   p
 
 let get_value_int s =
-  Dom.appendChild doc##.body (create_p (string_of_float s#value))
+  let v = G.number_value s in
+  Dom.appendChild doc##.body (create_p (string_of_float v))
 
 let get_value_str s =
-  Dom.appendChild doc##.body (create_p s#value)
+  let v = G.string_value s in
+  Dom.appendChild doc##.body (create_p v)
 
-let get_value_date s =
+let get_value_date (s : G.date_pattern) =
+  let utc = G.date_pattern_utc_offset s in
+  let dst = G.date_pattern_dst_offset s in
   let str =
-    "Pattern: " ^ s#pattern ^ "<br />" ^
-    "Timezone: " ^ s#timezone ^ "<br />" ^
-    "Utc_offset: " ^ (string_of_int s#utc_offset) ^ "<br />" ^
-    "dst_offset: " ^ (string_of_int s#dst_offset)
+    "Pattern: " ^ (G.date_pattern s) ^ "<br />" ^
+    "Timezone: " ^ (G.date_pattern_timezone s) ^ "<br />" ^
+    "Utc_offset: " ^ (string_of_int utc) ^ "<br />" ^
+    "dst_offset: " ^ (string_of_int dst)
   in
   Dom.appendChild doc##.body (create_p str)
 
 let get_value_currency s =
+  let fraction = G.currency_pattern_fraction s in
+  let rounding = G.currency_pattern_rounding s in
   let str =
-    "Pattern: " ^ s#pattern ^ "<br />" ^
-    "Code: " ^ s#code ^ "<br />" ^
-    "Fraction: " ^ (string_of_int s#fraction) ^ "<br />" ^
-    "Rounding: " ^ (string_of_int s#rounding) ^ "<br />" ^
-    "Decimal: " ^ s#decimal ^ "<br />" ^
-    "Grouping: " ^ s#grouping
+    "Pattern: " ^ (G.currency_pattern s) ^ "<br />" ^
+    "Code: " ^ (G.currency_pattern_code s) ^ "<br />" ^
+    "Fraction: " ^ (string_of_int fraction) ^ "<br />" ^
+    "Rounding: " ^ (string_of_int rounding) ^ "<br />" ^
+    "Decimal: " ^ (G.currency_pattern_decimal s) ^ "<br />" ^
+    "Grouping: " ^ (G.currency_pattern_grouping s)
   in
   Dom.appendChild doc##.body (create_p str)
 
 let on_device_ready () =
-  let g = Cordova_globalization.t () in
-  g#get_preferred_language
+  G.get_preferred_language
     get_value_str
     (fun () -> Jsoo_lib.console_log "Error");
 
-  g#get_locale_name get_value_str
+  G.get_locale_name get_value_str
     (fun () -> Jsoo_lib.console_log "Error");
 
-  g#date_to_string
+  G.date_to_string
     (Js_date.now ())
     get_value_str
     (fun () -> Jsoo_lib.console_log "Error")
     ();
 
-  g#get_first_day_of_week
+  G.get_first_day_of_week
     get_value_int
     (fun () -> Jsoo_lib.console_log "Error");
 
-  g#get_date_pattern
+  G.get_date_pattern
     get_value_date
     (fun () -> Jsoo_lib.console_log "Error")
     ();
 
-  g#get_currency_pattern
+  G.get_currency_pattern
     "USD"
     get_value_currency
     (fun () -> Jsoo_lib.console_log "Error")
 
-let _ = Cordova.Event.device_ready on_device_ready
+let _ =
+  Cordova.Event.device_ready on_device_ready
